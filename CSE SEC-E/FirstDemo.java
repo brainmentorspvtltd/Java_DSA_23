eb3a3603f0e0fe4d0c1c113764216043221aed01
Vivek Kumar <76239891+VivekKumar432@users.noreply.github.com>
2023-09-20 16:56:40 +0530

    Create FirstDemo.java

---
+++ CSE SEC-E/FirstDemo.java
@@ -0,0 +1,17 @@
+// Pascle case "FirstDemo"
+//Camel Case "firstDemo"
+//snake case or kebab case "first_demo"
+
+//to convert source code into byte code we have a commnad javac <SourceFileName.java>
+// to convert byte code into machine code we hava a command java <ByteCodeFileName>
+public class FirstDemo {
+    public static void main(String[] args) {
+        //print() println()
+        System.out.print("This is the main");
+        main();
+    }
+
+    public static void main() {
+        System.out.println("this is user defined main");
+    }
+}
