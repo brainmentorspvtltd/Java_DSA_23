bf249faf396962a244907d800c1b8012bdedf4b7
Vivek Kumar <76239891+VivekKumar432@users.noreply.github.com>
2023-09-20 16:55:52 +0530

    Create FirstDemo.java

---
+++ CSE SEC-D/FirstDemo.java
@@ -0,0 +1,18 @@
+// pascle case "FirstWord"
+// camel case "firstWord"
+// snake_case or kebab case " first_word"
+// for constant values we always use CAPITAL letter eg int HEIGHT = 50;
+
+
+
+//what is public.
+
+// to generate byte code from source code we have a command javac <JavaSourceFileName.java>
+// to generate machcine code from byte code we have a comamnd java <ByteCodeFileName>
+public class FirstDemo{
+    // what is static?
+    
+    public static void main(String[] args){
+
+    }
+}
