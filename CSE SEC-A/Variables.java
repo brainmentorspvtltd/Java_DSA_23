4af75a053abc541a7e304b35712e33b3e4ba75f9
Vivek Kumar <76239891+VivekKumar432@users.noreply.github.com>
2023-09-20 16:45:39 +0530

    Create Variables.java

---
+++ CSE SEC-A/Variables.java
@@ -0,0 +1,35 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 22; // here age is the name of the variable and 'int' is the data type of varaible a
+
+        byte age2 = 22;
+        byte age3 = (byte) 130;
+        // print() println()
+        // System.out.println("A");
+        // System.out.print("B");
+        // float percent = 10.25f;
+        // float percent = 10.25F;
+        float percent = (float) 10.25;
+        boolean isFemale = false;
+
+        char gender = 'M';
+        char gender2 = 'F';
+
+        // System.out.println(jap); // to be checked
+
+        String name = "Dam"; // String object literal
+        String name2 = name;
+        String name3 = "ABCD";
+        String name4 = new String("Aman");
+
+        // System.out.println(name == name2);
+        // System.out.println(name == name3);
+        // System.out.println(name == name4);
+        // System.out.println(name.equals(name4));
+        // System.out.println(name.equalsIgnoreCase(name4));
+
+        System.out.println(name4.compareTo(name));
+
+    }
+}
