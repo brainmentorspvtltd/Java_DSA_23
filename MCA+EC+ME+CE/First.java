ff574e1b15af7d32ba5df6ea3d636bd9f00ea66b
Vivek Kumar <76239891+VivekKumar432@users.noreply.github.com>
2023-09-23 15:30:33 +0530

    Add files via upload

---
+++ MCA+EC+ME+CE/First.java
@@ -0,0 +1,18 @@
+public class First {
+    public static void main(String[] deepanshu) {
+        int sum = 0;// here sum is the name of the variable and int is the datatype for variable sum
+
+        for (int a = 0; a < deepanshu.length; a++) {
+
+            String deep = deepanshu[a];
+            sum = sum + Integer.parseInt(deep);
+        }
+
+        System.out.println("This is sum of CMD line args :" + sum);
+        // int num1 = Integer.parseInt(args[0]);
+        // int num2 = Integer.parseInt(args[1]);
+        // int sum = num1 + num2;
+        // System.out.println("This is the sum of cmd line args :" + sum);
+
+    }
+}
---
+++ MCA+EC+ME+CE/Variable.java
@@ -0,0 +1,7 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte age = 22;
+        byte age2 = (byte)130;
+        System.out.println("This is the age :"+age2);
+    }
+}
