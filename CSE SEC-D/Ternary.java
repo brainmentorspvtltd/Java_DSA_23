c14ff646f4a9b64fb5fecb486dd364de1105b56e
Vivek Kumar <76239891+VivekKumar432@users.noreply.github.com>
2023-09-28 14:44:07 +0530

    Add files via upload

---
+++ CSE SEC-D/Ternary.java
@@ -0,0 +1,6 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int age = 12;
+        System.out.println((age >= 18) ? "Yes, you can vote" : "NO, you cannot vote");
+    }
+}
