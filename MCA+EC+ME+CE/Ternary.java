a617517021b850b7b11ef7e215a5b9bcfb83602a
Vivek Kumar <76239891+VivekKumar432@users.noreply.github.com>
2023-09-28 14:48:27 +0530

    Add files via upload

---
+++ MCA+EC+ME+CE/Ternary.java
@@ -0,0 +1,13 @@
+public class temp {
+    public static void main(String... args) {
+        // main("private", args);
+
+        int age =10;
+        System.out.println((age>=18)? "Yes. you can vote":"no, you cannot vote");
+    }
+
+    // private static void main(String type, String[] args) {
+    // System.out.println(args[0] + args[1]);
+        
+    // }
+}
