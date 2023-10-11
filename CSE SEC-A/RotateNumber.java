08bc05c2e2fe487cf00d9de6a95213de16867dee
Vivek Kumar <76239891+VivekKumar432@users.noreply.github.com>
2023-10-11 10:51:59 +0530

    Add files via upload

---
+++ CSE SEC-A/RotateNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class RotateNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int num = scanner.nextInt();
+        System.out.println("Please enter the number of rotations");
+        int rotations = scanner.nextInt();
+        int countDigit = 0;
+        int numCopy = num;
+        while (numCopy != 0) {
+            numCopy /= 10;
+            countDigit++;
+        }
+        rotations = rotations % countDigit;
+        if (rotations < 0) {
+            rotations = rotations + countDigit;
+        }
+        int power = (int) Math.pow(10, rotations);
+        int leftPart = num / power;
+        int rightPart = num % power;
+        int remainingPower = countDigit - rotations;
+        power = (int) Math.pow(10, remainingPower);
+
+        int res = rightPart * power + leftPart;
+
+        System.out.println("This is the result :" + res);
+        scanner.close();
+
+    }
+}
