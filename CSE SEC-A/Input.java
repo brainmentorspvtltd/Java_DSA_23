0c2ad3770484bf9d7f20526ed85fb67d6e515876
Vivek Kumar <76239891+VivekKumar432@users.noreply.github.com>
2023-10-18 10:57:17 +0530

    Create Input.java

---
+++ CSE SEC-A/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) throws Exception {
+
+        int ch = System.in.read();
+        char ch2 = (char) ch;
+        Scanner scanner = new Scanner(System.in);
+        scanner.next().charAt(0);
+
+    }
+}
